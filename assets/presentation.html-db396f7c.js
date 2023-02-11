ba051fc612a482e99f2046a0e003c50c52c863d0
Mister-Hope <mister-hope@outlook.com>
2023-02-11 13:01:09 +0000

    Deploying to netlify from @ vuepress-theme-hope/vuepress-theme-hope@331ccee79bce7b4b9da94517001c97dd9138e867 🚀

---
+++ assets/presentation.html-db396f7c.js
@@ -0,0 +1,69 @@
+import{a4 as m,_ as h,$ as k,a5 as c,a0 as s,a1 as t,a2 as n,a3 as e,a6 as l,k as i}from"./framework-bb5a5b49.js";const b={},g=n("p",null,"Пусть файл Markdown на вашем сайте VuePress поддерживает презентацию.",-1),_=n("h2",{id:"конфиг",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#конфиг","aria-hidden":"true"},"#"),e(" Конфиг")],-1),f=n("div",{class:"language-typescript line-numbers-mode","data-ext":"ts"},[n("pre",{class:"language-typescript"},[n("code",null,[n("span",{class:"token comment"},"// .vuepress/config.ts"),e(`
+`),n("span",{class:"token keyword"},"import"),e(),n("span",{class:"token punctuation"},"{"),e(" defineUserConfig "),n("span",{class:"token punctuation"},"}"),e(),n("span",{class:"token keyword"},"from"),e(),n("span",{class:"token string"},'"vuepress"'),n("span",{class:"token punctuation"},";"),e(`
+`),n("span",{class:"token keyword"},"import"),e(),n("span",{class:"token punctuation"},"{"),e(" hopeTheme "),n("span",{class:"token punctuation"},"}"),e(),n("span",{class:"token keyword"},"from"),e(),n("span",{class:"token string"},'"vuepress-theme-hope"'),n("span",{class:"token punctuation"},";"),e(`
+
+`),n("span",{class:"token keyword"},"export"),e(),n("span",{class:"token keyword"},"default"),e(),n("span",{class:"token function"},"defineUserConfig"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"{"),e(`
+  theme`),n("span",{class:"token operator"},":"),e(),n("span",{class:"token function"},"hopeTheme"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"{"),e(`
+    plugins`),n("span",{class:"token operator"},":"),e(),n("span",{class:"token punctuation"},"{"),e(`
+      mdEnhance`),n("span",{class:"token operator"},":"),e(),n("span",{class:"token punctuation"},"{"),e(`
+        presentation`),n("span",{class:"token operator"},":"),e(),n("span",{class:"token boolean"},"true"),n("span",{class:"token punctuation"},","),e(`
+      `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),e(`
+    `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),e(`
+  `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},","),e(`
+`),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},";"),e(`
+`)])]),n("div",{class:"highlight-lines"},[n("br"),n("br"),n("br"),n("br"),n("br"),n("br"),n("br"),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("br"),n("br"),n("br")]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),w=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token comment"},"// .vuepress/config.js"),e(`
+`),n("span",{class:"token keyword"},"import"),e(),n("span",{class:"token punctuation"},"{"),e(" hopeTheme "),n("span",{class:"token punctuation"},"}"),e(),n("span",{class:"token keyword"},"from"),e(),n("span",{class:"token string"},'"vuepress-theme-hope"'),n("span",{class:"token punctuation"},";"),e(`
+
+`),n("span",{class:"token keyword"},"export"),e(),n("span",{class:"token keyword"},"default"),e(),n("span",{class:"token punctuation"},"{"),e(`
+  `),n("span",{class:"token literal-property property"},"theme"),n("span",{class:"token operator"},":"),e(),n("span",{class:"token function"},"hopeTheme"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"{"),e(`
+    `),n("span",{class:"token literal-property property"},"plugins"),n("span",{class:"token operator"},":"),e(),n("span",{class:"token punctuation"},"{"),e(`
+      `),n("span",{class:"token literal-property property"},"mdEnhance"),n("span",{class:"token operator"},":"),e(),n("span",{class:"token punctuation"},"{"),e(`
+        `),n("span",{class:"token literal-property property"},"presentation"),n("span",{class:"token operator"},":"),e(),n("span",{class:"token boolean"},"true"),n("span",{class:"token punctuation"},","),e(`
+      `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),e(`
+    `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),e(`
+  `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},","),e(`
+`),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},";"),e(`
+`)])]),n("div",{class:"highlight-lines"},[n("br"),n("br"),n("br"),n("br"),n("br"),n("br"),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("br"),n("br"),n("br")]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),y=n("code",null,"vuepress-plugin-md-enhance",-1),j={href:"https://revealjs.com/",target:"_blank",rel:"noopener noreferrer"},x=l('<p>Вы также можете передать объект для настройки.</p><p><code>presentation.plugins</code> получает массив строк, что позволяет вам свободно настраивать, включать ли некоторые предустановленные плагины.</p><div class="hint-container tip"><p class="hint-container-title">Совет</p><p>Приемлемые плагины:</p><ul><li><code>&quot;highlight&quot;</code></li><li><code>&quot;math&quot;</code></li><li><code>&quot;search&quot;</code></li><li><code>&quot;notes&quot;</code></li><li><code>&quot;zoom&quot;</code></li></ul></div><p>Вы также можете использовать параметры конфигурации <code>presentation.revealConfig</code>, передаваемые в Reveal.js глобально.</p>',4),q={href:"https://github.com/hakimel/reveal.js/wiki/Plugins,-Tools-and-Hardware",target:"_blank",rel:"noopener noreferrer"},L={href:"https://github.com/vuepress-theme-hope/vuepress-theme-hope/issues/new?assignees=Mister-Hope&labels=enhancement&template=feature_request.md&title=%5BFeature+Request%5D",target:"_blank",rel:"noopener noreferrer"},E=l(`<h2 id="синтаксис" tabindex="-1"><a class="header-anchor" href="#синтаксис" aria-hidden="true">#</a> Синтаксис</h2><ul><li>Используйте <code>---</code> для разделения слайдов</li><li>Используйте <code>--</code>, чтобы разделить слайды во второй раз (вертикальное отображение)</li></ul><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>@slidestart [theme]
+
+<span class="token comment">&lt;!-- slide1 --&gt;</span>
+
+<span class="token hr punctuation">---</span>
+
+<span class="token comment">&lt;!-- slide2 --&gt;</span>
+
+<span class="token hr punctuation">---</span>
+
+<span class="token comment">&lt;!-- slide3 --&gt;</span>
+
+@slideend
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>Доступная тема (замените <code>[theme]</code> на них):</p><ul><li><code>auto</code> (По умолчанию)</li><li><code>black</code></li><li><code>white</code></li><li><code>league</code></li><li><code>beige</code></li><li><code>sky</code></li><li><code>night</code></li><li><code>serif</code></li><li><code>simple</code></li><li><code>solarized</code></li><li><code>blood</code></li><li><code>moon</code></li></ul>`,5),C=n("h2",{id:"демо",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#демо","aria-hidden":"true"},"#"),e(" Демо")],-1),V=l(`<div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>@slidestart
+
+<span class="token title important"><span class="token punctuation">##</span> Слайд 1</span>
+
+A paragraph with some text and a <span class="token url">[<span class="token content">link</span>](<span class="token url">https://mrhope.site</span>)</span>
+
+<span class="token hr punctuation">---</span>
+
+<span class="token title important"><span class="token punctuation">##</span> Слайд 2</span>
+
+<span class="token list punctuation">-</span> Элемент 1
+<span class="token list punctuation">-</span> Элемент 2
+
+<span class="token hr punctuation">---</span>
+
+<span class="token title important"><span class="token punctuation">##</span> Слайд 3.1</span>
+
+<span class="token code"><span class="token punctuation">\`\`\`</span><span class="token code-language">js</span>
+<span class="token code-block language-js language-js"><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span></span>
+<span class="token punctuation">\`\`\`</span></span>
+
+--
+
+<span class="token title important"><span class="token punctuation">##</span> Слайд 3.2</span>
+
+$$
+J(\\theta_0,\\theta_1) = \\sum_{i=0}
+$$
+
+@slideend
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1),T={class:"hint-container info"},U=n("p",{class:"hint-container-title"},"Инфо",-1),B=n("h2",{id:"опции",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#опции","aria-hidden":"true"},"#"),e(" Опции")],-1),K=n("p",null,[e("Вы можете установить "),n("code",null,"reveal"),e(" для передачи параметров в reveal.js для каждой страницы во фронтмейтере, вы также можете установить "),n("code",null,"presentation"),e(" в параметрах плагина, чтобы установить reveal.js глобально.")],-1),N={href:"https://revealjs.com/config/",target:"_blank",rel:"noopener noreferrer"},P={href:"https://revealjs.com/",target:"_blank",rel:"noopener noreferrer"};function S(H,F){const r=i("CodeTabs"),a=i("ExternalLinkIcon"),o=i("ProjectLink"),p=i("Presentation");return h(),k("div",null,[g,c(" more "),c("lint disable no-duplicate-headings"),_,s(r,{id:"8",data:[{title:"TS"},{title:"JS"}],"tab-id":"language"},{tab0:t(({title:d,value:u,isActive:v})=>[f]),tab1:t(({title:d,value:u,isActive:v})=>[w]),_:1}),n("p",null,[y,e(" использует "),n("a",j,[e("reveal.js"),s(a)]),e(" для поддержки этой функции.")]),x,n("p",null,[e("Reveal.js также предоставляет "),n("a",q,[e("больше плагинов"),s(a)]),e(". Если вам нужен конкретный плагин, отправьте "),n("a",L,[e("Запрос функции"),s(a)]),e(" на GitHub.")]),E,n("p",null,[e("Для получения дополнительной информации смотрите "),s(o,{name:"md-enhance",path:"/guide/presentation/themes.html"},{default:t(()=>[e("Демо темы")]),_:1}),e(".")]),C,s(p,{id:"presentation-150",code:"eJzjUlZWuLDwwu4LGy7svLBFwZCL68KECxsvbL+w4WKbwsVGhYtNF7Ze2HWxEUjvu7BH4cIOhWggp/FiN1DLrgsbYjUySkoKiq309XOLMvILUvWKM0tSNbm4dHV1udCMNgKKKlxYC+RvvbAHiPdebAJahyEEUoap2VgP6LKEhISsYq7k/LziEoVEBVsFQ2uQEEg9pnKgMSoqXF4aMSUZqSWJ8QY6UIahJlBjTHFpbnx1pq1BLUgRAPIpcmg=",theme:"auto"}),V,n("div",T,[U,n("p",null,[e("Подробную демонстрацию смотрите в разделе "),s(o,{name:"md-enhance",path:"/guide/presentation/demo.html"},{default:t(()=>[e("Демонстрационная презентация")]),_:1}),e(".")])]),B,K,n("p",null,[e("Дополнительные параметры смотрите в "),n("a",N,[e("конфигурации reveal.js"),s(a)]),e(". Дополнительные сведения об использовании смотрите в "),n("a",P,[e("документации reveal.js"),s(a)])])])}const M=m(b,[["render",S],["__file","presentation.html.vue"]]);export{M as default};
