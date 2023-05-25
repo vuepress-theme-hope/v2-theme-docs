a1d12dac4c8ff4565db6ea4c1d9a45d5c9e44799
Mister-Hope <mister-hope@outlook.com>
2023-05-25 07:48:04 +0000

    Deploying to netlify from @ vuepress-theme-hope/vuepress-theme-hope@47c1b6fdc8e09ece286bd7d5f809ca46aedafd5d 🚀

---
+++ assets/container.html-6557f4fe.js
@@ -0,0 +1,131 @@
+import{_ as c,r as l,o,c as p,f as d,a as r,w as a,b as s,e as n,g as u}from"./app-a38c4918.js";const v={},m=n("p",null,"The theme adds tip, note, info, warning, danger and detail container.",-1),k=n("h2",{id:"config",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#config","aria-hidden":"true"},"#"),s(" Config")],-1),b=n("div",{class:"language-typescript line-numbers-mode","data-ext":"ts"},[n("pre",{ts:"",class:"language-typescript"},[n("code",null,[n("span",{class:"token comment"},"// .vuepress/config.ts"),s(`
+`),n("span",{class:"token keyword"},"import"),s(),n("span",{class:"token punctuation"},"{"),s(" defineUserConfig "),n("span",{class:"token punctuation"},"}"),s(),n("span",{class:"token keyword"},"from"),s(),n("span",{class:"token string"},'"vuepress"'),n("span",{class:"token punctuation"},";"),s(`
+`),n("span",{class:"token keyword"},"import"),s(),n("span",{class:"token punctuation"},"{"),s(" hopeTheme "),n("span",{class:"token punctuation"},"}"),s(),n("span",{class:"token keyword"},"from"),s(),n("span",{class:"token string"},'"vuepress-theme-hope"'),n("span",{class:"token punctuation"},";"),s(`
+
+`),n("span",{class:"token keyword"},"export"),s(),n("span",{class:"token keyword"},"default"),s(),n("span",{class:"token function"},"defineUserConfig"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"{"),s(`
+  theme`),n("span",{class:"token operator"},":"),s(),n("span",{class:"token function"},"hopeTheme"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"{"),s(`
+    plugins`),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"{"),s(`
+      mdEnhance`),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"{"),s(`
+        `),n("span",{class:"token comment"},"// this is the default option, so you can use it directly"),s(`
+        container`),n("span",{class:"token operator"},":"),s(),n("span",{class:"token boolean"},"true"),n("span",{class:"token punctuation"},","),s(`
+      `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+    `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+  `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},","),s(`
+`),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},";"),s(`
+`)])]),n("div",{class:"highlight-lines"},[n("br"),n("br"),n("br"),n("br"),n("br"),n("br"),n("br"),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("br"),n("br"),n("br")]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),h=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{js:"",class:"language-javascript"},[n("code",null,[n("span",{class:"token comment"},"// .vuepress/config.js"),s(`
+`),n("span",{class:"token keyword"},"import"),s(),n("span",{class:"token punctuation"},"{"),s(" hopeTheme "),n("span",{class:"token punctuation"},"}"),s(),n("span",{class:"token keyword"},"from"),s(),n("span",{class:"token string"},'"vuepress-theme-hope"'),n("span",{class:"token punctuation"},";"),s(`
+
+`),n("span",{class:"token keyword"},"export"),s(),n("span",{class:"token keyword"},"default"),s(),n("span",{class:"token punctuation"},"{"),s(`
+  `),n("span",{class:"token literal-property property"},"theme"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token function"},"hopeTheme"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token literal-property property"},"plugins"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"{"),s(`
+      `),n("span",{class:"token literal-property property"},"mdEnhance"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"{"),s(`
+        `),n("span",{class:"token comment"},"// this is the default option, so you can use it directly"),s(`
+        `),n("span",{class:"token literal-property property"},"container"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token boolean"},"true"),n("span",{class:"token punctuation"},","),s(`
+      `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+    `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+  `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},","),s(`
+`),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},";"),s(`
+`)])]),n("div",{class:"highlight-lines"},[n("br"),n("br"),n("br"),n("br"),n("br"),n("br"),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("br"),n("br"),n("br")]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),g=u(`<h2 id="demo" tabindex="-1"><a class="header-anchor" href="#demo" aria-hidden="true">#</a> Demo</h2><div class="hint-container info"><p class="hint-container-title">Info</p><p>Information container</p></div><div class="hint-container note"><p class="hint-container-title">Note</p><p>Note container</p></div><div class="hint-container tip"><p class="hint-container-title">Tips</p><p>Tip container</p></div><div class="hint-container warning"><p class="hint-container-title">Note</p><p>Warning container</p></div><div class="hint-container danger"><p class="hint-container-title">Warning</p><p>Dangerous container</p></div><details class="hint-container details"><summary>Details</summary><p>Details container</p></details><div class="hint-container info"><p class="hint-container-title">Custom Title</p><p>A custom information container with <code>code</code>, <a href="#demo">link</a>.</p><div class="language-javascript" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre></div></div><div class="hint-container note"><p class="hint-container-title">Custom Title</p><p>A custom note container with <code>code</code>, <a href="#demo">link</a>.</p><div class="language-javascript" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre></div></div><div class="hint-container tip"><p class="hint-container-title">Custom Title</p><p>A custom tip container with <code>code</code>, <a href="#demo">link</a>.</p><div class="language-javascript" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre></div></div><div class="hint-container warning"><p class="hint-container-title">Custom Title</p><p>A custom warning container with <code>code</code>, <a href="#demo">link</a>.</p><div class="language-javascript" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre></div></div><div class="hint-container danger"><p class="hint-container-title">Custom Title</p><p>A custom danger container with <code>code</code>, <a href="#demo">link</a>.</p><div class="language-javascript" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre></div></div><details class="hint-container details"><summary>Custom Title</summary><p>A custom details container with <code>code</code>, <a href="#demo">link</a>.</p><div class="language-javascript" data-ext="js"><pre class="language-javascript"><code><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span>
+</code></pre></div></details><div class="hint-container info"><p class="hint-container-title">Custom info</p></div><div class="hint-container note"><p class="hint-container-title">Custom note</p></div><div class="hint-container tip"><p class="hint-container-title">Custom tip</p></div><div class="hint-container warning"><p class="hint-container-title">Custom warning</p></div><div class="hint-container danger"><p class="hint-container-title">Custom danger</p></div><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>::: info
+Information container
+:::
+
+::: note
+Note container
+:::
+
+::: tip
+Tip container
+:::
+
+::: warning
+Warning container
+:::
+
+::: danger
+Dangerous container
+:::
+
+::: details
+Details container
+:::
+
+::: info Custom Title
+
+A custom information container with <span class="token code-snippet code keyword">\`code\`</span>, <span class="token url">[<span class="token content">link</span>](<span class="token url">#demo</span>)</span>.
+
+<span class="token code"><span class="token punctuation">\`\`\`</span><span class="token code-language">js</span>
+<span class="token code-block language-js"><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span></span>
+<span class="token punctuation">\`\`\`</span></span>
+
+:::
+
+::: note Custom Title
+A custom note container with <span class="token code-snippet code keyword">\`code\`</span>, <span class="token url">[<span class="token content">link</span>](<span class="token url">#demo</span>)</span>.
+
+<span class="token code"><span class="token punctuation">\`\`\`</span><span class="token code-language">js</span>
+<span class="token code-block language-js"><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span></span>
+<span class="token punctuation">\`\`\`</span></span>
+
+:::
+
+::: tip Custom Title
+
+A custom tip container with <span class="token code-snippet code keyword">\`code\`</span>, <span class="token url">[<span class="token content">link</span>](<span class="token url">#demo</span>)</span>.
+
+<span class="token code"><span class="token punctuation">\`\`\`</span><span class="token code-language">js</span>
+<span class="token code-block language-js"><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span></span>
+<span class="token punctuation">\`\`\`</span></span>
+
+:::
+
+::: warning Custom Title
+
+A custom warning container with <span class="token code-snippet code keyword">\`code\`</span>, <span class="token url">[<span class="token content">link</span>](<span class="token url">#demo</span>)</span>.
+
+<span class="token code"><span class="token punctuation">\`\`\`</span><span class="token code-language">js</span>
+<span class="token code-block language-js"><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span></span>
+<span class="token punctuation">\`\`\`</span></span>
+
+:::
+
+::: danger Custom Title
+
+A custom danger container with <span class="token code-snippet code keyword">\`code\`</span>, <span class="token url">[<span class="token content">link</span>](<span class="token url">#demo</span>)</span>.
+
+<span class="token code"><span class="token punctuation">\`\`\`</span><span class="token code-language">js</span>
+<span class="token code-block language-js"><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span></span>
+<span class="token punctuation">\`\`\`</span></span>
+
+:::
+
+::: details Custom Title
+
+A custom details container with <span class="token code-snippet code keyword">\`code\`</span>, <span class="token url">[<span class="token content">link</span>](<span class="token url">#demo</span>)</span>.
+
+<span class="token code"><span class="token punctuation">\`\`\`</span><span class="token code-language">js</span>
+<span class="token code-block language-js"><span class="token keyword">const</span> a <span class="token operator">=</span> <span class="token number">1</span><span class="token punctuation">;</span></span>
+<span class="token punctuation">\`\`\`</span></span>
+
+:::
+
+::: info Custom info
+:::
+
+::: note Custom note
+:::
+
+::: tip Custom tip
+:::
+
+::: warning Custom warning
+:::
+
+::: danger Custom danger
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,19);function w(f,y){const t=l("CodeTabs");return o(),p("div",null,[m,d(" more "),k,r(t,{id:"7",data:[{id:"TS"},{id:"JS"}],"tab-id":"language"},{title0:a(({value:e,isActive:i})=>[s("TS")]),title1:a(({value:e,isActive:i})=>[s("JS")]),tab0:a(({value:e,isActive:i})=>[b]),tab1:a(({value:e,isActive:i})=>[h]),_:1}),g])}const C=c(v,[["render",w],["__file","container.html.vue"]]);export{C as default};
