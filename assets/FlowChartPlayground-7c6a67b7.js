447e4df0f5149990f14b64de3825e0a2f0cc245c
Mister-Hope <mister-hope@outlook.com>
2023-05-31 06:26:00 +0000

    Deploying to netlify from @ vuepress-theme-hope/vuepress-theme-hope@8a3bee297b0b6fde1b9f27a78652f5396e7ed0b2 🚀

---
+++ assets/FlowChartPlayground-7c6a67b7.js
@@ -0,0 +1,16 @@
+import{d as w,v as f,x as h,B as i,y as g,K as y,Q as d,R as m,S as b,U as P,k as t}from"./app-d7067868.js";const u="flowchart-playground",_=`st=>start: Start|past:>http://www.google.com[blank]
+e=>end: End|future:>http://www.google.com
+op1=>operation: My Operation|past
+op2=>operation: Stuff|current
+sub1=>subroutine: My Subroutine|invalid
+cond=>condition: Yes
+or No?|approved:>http://www.google.com
+c2=>condition: Good idea|rejected
+io=>inputoutput: catch something...|future
+
+st->op1(right)->cond
+cond(yes, right)->c2
+cond(no)->sub1(left)->op1
+c2(yes)->io->e
+c2(no)->op2->e
+`,F={"/":{config:"Flowchart Config",preset:"Flowchart Preset",result:"Render Result"},"/zh/":{config:"流程图配置",preset:"流程图预设",result:"渲染结果"}},R=w({name:"FlowChartPlayground",setup(){const s=f(F);let l=null;const p=h(),r=i(_),a=i("vue"),n=i(1),c=e=>e<419?.8:e>1280?1:.9;return g(()=>{let e=null;Promise.all([y(()=>import("./flowchart-d65a1d8e.js"),[]),new Promise(o=>setTimeout(o,800))]).then(([{parse:o}])=>{e=o;try{l=o(r.value),n.value=c(window.innerWidth),l.draw(u,{...d[a.value],scale:n.value})}catch(v){console.log(v)}}),m([r,a],()=>{if(e)try{l=e(r.value),n.value=c(window.innerWidth),p.value.innerHTML="",l.draw(u,{...d[a.value],scale:n.value})}catch(o){console.log(o)}}),b("resize",P(()=>{if(l){const o=c(window.innerWidth);n.value!==o&&(n.value=o,l.draw(u,{...d[a.value],scale:o}))}},100))}),()=>t("div",{class:"flowchart-playground"},[t("label",{for:"flowchart-playground-config"},`${s.value.config}:`),t("textarea",{id:"flowchart-playground-config",value:r.value,onInput:({target:e})=>{r.value=e.value}}),t("div",[t("label",{for:"flowchart-playground-preset"},`${s.value.preset}:`),t("select",{id:"flowchart-playground-preset",value:a.value,onChange:({target:e})=>{a.value=e.value}},["ant","pie","vue"].map(e=>t("option",{value:e},e)))]),t("label",{for:u},`${s.value.result}:`),t("div",{ref:p,class:["flowchart-wrapper",a.value],id:u})])}});export{R as default};
---
+++ assets/pure.html-7be86c1b.js
@@ -0,0 +1,1 @@
+import{_ as o,o as c,c as s,e,f as t}from"./app-d7067868.js";const _={},n=e("p",null,[t("如果你的网站是一个纯文档站点，并且你更喜欢干净的样式，你可以在主题选项中设置 "),e("code",null,"pure: true"),t(" 启用纯净模式。")],-1),r=e("p",null,"在这个模式下，我们会禁用一些花哨的动画以及一些色彩，只提供功能。",-1),l=[n,r];function a(u,d){return c(),s("div",null,l)}const f=o(_,[["render",a],["__file","pure.html.vue"]]);export{f as default};
