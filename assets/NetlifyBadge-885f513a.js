f47c6e23a65b8071c850f12d4679e41398864eb6
Mister-Hope <mister-hope@outlook.com>
2023-06-01 07:54:41 +0000

    Deploying to netlify from @ vuepress-theme-hope/vuepress-theme-hope@11d7ccfc8e80db39291a2d29fb1f617671c2acff 🚀

---
+++ assets/NetlifyBadge-885f513a.js
@@ -0,0 +1,1 @@
+import{d as l,j as s,k as t,C as i}from"./app-25dbeecb.js";const o=l({name:"NetlifyBadge",props:{alt:{type:String,default:"Deploys by Netlify"}},setup(e){const{isDarkmode:a}=s();return()=>t(i,()=>t("a",{href:"https://www.netlify.com",target:"_blank"},t("img",{src:`https://www.netlify.com/img/global/badges/netlify-${a.value?"dark":"light"}.svg`,alt:e.alt})))}});export{o as N};
