600fdf4ece0e073fae9efb18c6156a6ccf83e176
Mister-Hope <mister-hope@outlook.com>
2023-06-07 12:46:27 +0000

    Deploying to netlify from @ vuepress-theme-hope/vuepress-theme-hope@9cf1f2af31e8cf392f599abb464d7ec1f53dd247 🚀

---
+++ assets/provider-KHKJ2PBP-b438573a.js
@@ -0,0 +1,1 @@
+import{f as t}from"./register-FEQMQZNK-baa1adc2.js";import"./app-0ba868ac.js";var i=Symbol(0),r,u=class extends t{constructor(){super(...arguments),this[r]=!0}get type(){return"audio"}setup(e){super.setup(e),this.type==="audio"&&e.delegate.d("provider-setup",{detail:this})}get audio(){return this.a}};r=i;export{i as AUDIO_PROVIDER,u as AudioProvider};
---
+++ assets/pure.html-e403e9b6.js
@@ -0,0 +1,1 @@
+import{_ as o,o as c,c as s,b as e,d as t}from"./app-0ba868ac.js";const _={},n=e("p",null,[t("如果你的网站是一个纯文档站点，并且你更喜欢干净的样式，你可以在主题选项中设置 "),e("code",null,"pure: true"),t(" 启用纯净模式。")],-1),r=e("p",null,"在这个模式下，我们会禁用一些花哨的动画以及一些色彩，只提供功能。",-1),l=[n,r];function a(u,d){return c(),s("div",null,l)}const i=o(_,[["render",a],["__file","pure.html.vue"]]);export{i as default};
