8319260254ce187aad81f2748c0d6fc0739688ca
Mister-Hope <mister-hope@outlook.com>
2023-07-04 05:03:25 +0000

    Deploying to netlify from @ vuepress-theme-hope/vuepress-theme-hope@99282446bb90e8c23ccd5723fc85ca48f8baffbd 🚀

---
+++ assets/file.html-ddc020cb.js
@@ -0,0 +1,22 @@
+import{_ as e,o as i,c as n,f as s}from"./app-babce519.js";const l={},d=s(`<h2 id="file-structure" tabindex="-1"><a class="header-anchor" href="#file-structure" aria-hidden="true">#</a> File Structure</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>.
+├── docs → Docs folder depending on you
+│    │
+│    ├── .vuepress (Optional) → VuePress Config
+│    │     │
+│    │     ├── dist (Default) → Output folder
+│    │     │
+│    │     │
+│    │     ├── public (Optional) → Static resource dir
+│    │     │
+│    │     ├── styles (Optional) → Stores style related files
+│    │     │
+│    │     ├── config.{js,ts} (Optional) → Entry file of configuration
+│    │     │
+│    │     └── client.{js,ts} (Optional) → Client App file
+│    │
+│    ├── ... → Other docs
+│    │
+│    └── README.md → Project homepage
+│
+└── package.json → Node.js declaration file for the entire project
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),r=[d];function a(t,c){return i(),n("div",null,r)}const u=e(l,[["render",a],["__file","file.html.vue"]]);export{u as default};
