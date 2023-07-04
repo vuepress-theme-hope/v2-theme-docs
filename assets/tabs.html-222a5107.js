0f2235407fadfb956d1a827a31d76672d09fff93
Mister-Hope <mister-hope@outlook.com>
2023-07-04 05:03:25 +0000

    Deploying to netlify from @ vuepress-theme-hope/vuepress-theme-hope@99282446bb90e8c23ccd5723fc85ca48f8baffbd 🚀

---
+++ assets/tabs.html-222a5107.js
@@ -0,0 +1,111 @@
+import{_ as u,r as d,o as r,c as p,a as v,e as l,w as e,d as s,b as n,f as c}from"./app-babce519.js";const b={},m=n("p",null,"让你的 Markdown 文件支持供选项卡。",-1),h=n("h2",{id:"配置",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#配置","aria-hidden":"true"},"#"),s(" 配置")],-1),k=n("div",{class:"language-typescript line-numbers-mode","data-ext":"ts"},[n("pre",{ts:"",class:"language-typescript"},[n("code",null,[n("span",{class:"token comment"},"// .vuepress/config.ts"),s(`
+`),n("span",{class:"token keyword"},"import"),s(),n("span",{class:"token punctuation"},"{"),s(" defineUserConfig "),n("span",{class:"token punctuation"},"}"),s(),n("span",{class:"token keyword"},"from"),s(),n("span",{class:"token string"},'"vuepress"'),n("span",{class:"token punctuation"},";"),s(`
+`),n("span",{class:"token keyword"},"import"),s(),n("span",{class:"token punctuation"},"{"),s(" hopeTheme "),n("span",{class:"token punctuation"},"}"),s(),n("span",{class:"token keyword"},"from"),s(),n("span",{class:"token string"},'"vuepress-theme-hope"'),n("span",{class:"token punctuation"},";"),s(`
+
+`),n("span",{class:"token keyword"},"export"),s(),n("span",{class:"token keyword"},"default"),s(),n("span",{class:"token function"},"defineUserConfig"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"{"),s(`
+  theme`),n("span",{class:"token operator"},":"),s(),n("span",{class:"token function"},"hopeTheme"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"{"),s(`
+    plugins`),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"{"),s(`
+      mdEnhance`),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"{"),s(`
+        `),n("span",{class:"token comment"},"// 添加选项卡支持"),s(`
+        tabs`),n("span",{class:"token operator"},":"),s(),n("span",{class:"token boolean"},"true"),n("span",{class:"token punctuation"},","),s(`
+      `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+    `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+  `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},","),s(`
+`),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},";"),s(`
+`)])]),n("div",{class:"highlight-lines"},[n("br"),n("br"),n("br"),n("br"),n("br"),n("br"),n("br"),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("br"),n("br"),n("br")]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),g=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{js:"",class:"language-javascript"},[n("code",null,[n("span",{class:"token comment"},"// .vuepress/config.js"),s(`
+`),n("span",{class:"token keyword"},"import"),s(),n("span",{class:"token punctuation"},"{"),s(" hopeTheme "),n("span",{class:"token punctuation"},"}"),s(),n("span",{class:"token keyword"},"from"),s(),n("span",{class:"token string"},'"vuepress-theme-hope"'),n("span",{class:"token punctuation"},";"),s(`
+
+`),n("span",{class:"token keyword"},"export"),s(),n("span",{class:"token keyword"},"default"),s(),n("span",{class:"token punctuation"},"{"),s(`
+  `),n("span",{class:"token literal-property property"},"theme"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token function"},"hopeTheme"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token literal-property property"},"plugins"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"{"),s(`
+      `),n("span",{class:"token literal-property property"},"mdEnhance"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"{"),s(`
+        `),n("span",{class:"token comment"},"// 添加选项卡支持"),s(`
+        `),n("span",{class:"token literal-property property"},"tabs"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token boolean"},"true"),n("span",{class:"token punctuation"},","),s(`
+      `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+    `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+  `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},","),s(`
+`),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},";"),s(`
+`)])]),n("div",{class:"highlight-lines"},[n("br"),n("br"),n("br"),n("br"),n("br"),n("br"),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("div",{class:"highlight-line"}," "),n("br"),n("br"),n("br")]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),_=c(`<h2 id="用法" tabindex="-1"><a class="header-anchor" href="#用法" aria-hidden="true">#</a> 用法</h2><p>你需要将选项卡包装在 <code>tabs</code> 容器中。</p><p>你可以在 <code>tabs</code> 容器中添加一个 id 后缀，该后缀将用作选项卡 id。 所有具有相同 id 的选项卡将共享相同的切换事件。</p><div class="language-markdown" data-ext="md"><pre class="language-markdown"><code>::: tabs#fruit
+
+<span class="token comment">&lt;!-- 这里，fruit 将用作 id，它是可选的 --&gt;</span>
+
+<span class="token comment">&lt;!-- 选项卡内容 --&gt;</span>
+
+:::
+</code></pre></div><p>在这个容器内，你应该使用 <code>@tab</code> 标记来标记和分隔选项卡内容。</p><p>在 <code>@tab</code> 标记后，你可以添加文本 <code>:active</code> 默认激活选项卡，之后的文本将被解析为选项卡标题。</p><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>::: tabs
+
+@tab 标题 1
+
+<span class="token comment">&lt;!-- tab 1 内容 --&gt;</span>
+
+@tab 标题 2
+
+<span class="token comment">&lt;!-- tab 2 内容 --&gt;</span>
+
+@tab:active 标题 3
+
+<span class="token comment">&lt;!-- tab 3 将会被默认激活 --&gt;</span>
+
+<span class="token comment">&lt;!-- tab 3 内容 --&gt;</span>
+
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>默认情况下，标题将用作选项卡的值，但你可以使用 id 后缀覆盖它。</p><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>::: tabs
+
+@tab 标题 1
+
+<span class="token comment">&lt;!-- 此处，选项卡 1 的标题“标题 1”将用作值。 --&gt;</span>
+
+<span class="token comment">&lt;!-- tab 1 内容 --&gt;</span>
+
+@tab 标题 2#值 2
+
+<span class="token comment">&lt;!-- 这里，tab 2 的标题将是 “标题 2”，但它会使用 “值 2” 作为选项卡的值--&gt;</span>
+
+<span class="token comment">&lt;!-- tab 2 内容 --&gt;</span>
+
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>你可以在每个选项卡中使用 Vue 语法和组件，并且你可以访问 <code>value</code> 和 <code>isActive</code>，表示选项卡的绑定值和选项卡是否处于激活状态。</p>`,10),f={class:"hint-container info"},A=n("p",{class:"hint-container-title"},"一起切换并保持选择",-1),w=n("p",null,"如果你想让一些选项卡组一起切换，你可以使用相同的选项卡 ID 来绑定它们。",-1),x=n("p",null,"针对每个选项卡 ID 的选择会被存储并进行持久化。",-1),y=n("p",null,"下方是一个案例。",-1),T=n("p",null,"选择包管理器:",-1),B=n("p",null,"npm 应该与 Node.js 被一同安装。",-1),C=n("div",{class:"language-bash","data-ext":"sh"},[n("pre",{class:"language-bash"},[n("code",null,[s("corepack prepare pnpm@latest "),n("span",{class:"token parameter variable"},"--activated"),s(`
+`)])])],-1),j=n("p",null,[s("安装 "),n("code",null,"vuepress-plugin-md-enhance"),s(":")],-1),N=n("div",{class:"language-bash","data-ext":"sh"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token function"},"npm"),s(" i "),n("span",{class:"token parameter variable"},"-D"),s(` vuepress-plugin-md-enhance
+`)])])],-1),V=n("div",{class:"language-bash","data-ext":"sh"},[n("pre",{class:"language-bash"},[n("code",null,[n("span",{class:"token function"},"pnpm"),s(),n("span",{class:"token function"},"add"),s(),n("span",{class:"token parameter variable"},"-D"),s(` vuepress-plugin-md-enhance
+`)])])],-1),S=n("h2",{id:"案例",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#案例","aria-hidden":"true"},"#"),s(" 案例")],-1),D=n("p",null,"一个水果选项卡列表:",-1),O=n("p",null,"Apple",-1),E=n("p",null,"Banana",-1),I=c(`<div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>::: tabs#fruit
+
+@tab apple#apple
+
+Apple
+
+@tab banana#banana
+
+Banana
+
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>另一个水果选项卡列表:</p>`,2),J=n("p",null,"Apple",-1),U=n("p",null,"Banana",-1),M=n("p",null,"Orange",-1),q=c(`<div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>::: tabs#fruit
+
+@tab apple
+
+Apple
+
+@tab banana
+
+Banana
+
+@tab orange
+
+Orange
+
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>一个没有绑定 id 的水果选项卡列表:</p>`,2),z=n("p",null,"Apple",-1),F=n("p",null,"Banana",-1),G=n("p",null,"Orange",-1),H=c(`<div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>::: tabs
+
+@tab apple
+
+Apple
+
+@tab banana
+
+Banana
+
+@tab orange
+
+Orange
+
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,1);function K(L,P){const o=d("CodeTabs"),t=d("Tabs");return r(),p("div",null,[m,v(" more "),h,l(o,{id:"7",data:[{id:"TS"},{id:"JS"}],"tab-id":"language"},{title0:e(({value:a,isActive:i})=>[s("TS")]),title1:e(({value:a,isActive:i})=>[s("JS")]),tab0:e(({value:a,isActive:i})=>[k]),tab1:e(({value:a,isActive:i})=>[g]),_:1}),_,n("div",f,[A,w,x,y,T,l(t,{id:"52",data:[{id:"npm"},{id:"pnpm"}],"tab-id":"shell"},{title0:e(({value:a,isActive:i})=>[s("npm")]),title1:e(({value:a,isActive:i})=>[s("pnpm")]),tab0:e(({value:a,isActive:i})=>[B]),tab1:e(({value:a,isActive:i})=>[C]),_:1}),j,l(t,{id:"65",data:[{id:"npm"},{id:"pnpm"}],"tab-id":"shell"},{title0:e(({value:a,isActive:i})=>[s("使用 npm")]),title1:e(({value:a,isActive:i})=>[s("使用 pnpm")]),tab0:e(({value:a,isActive:i})=>[N]),tab1:e(({value:a,isActive:i})=>[V]),_:1})]),S,D,l(t,{id:"80",data:[{id:"apple"},{id:"banana"}],"tab-id":"fruit"},{title0:e(({value:a,isActive:i})=>[s("apple")]),title1:e(({value:a,isActive:i})=>[s("banana")]),tab0:e(({value:a,isActive:i})=>[O]),tab1:e(({value:a,isActive:i})=>[E]),_:1}),I,l(t,{id:"96",data:[{id:"apple"},{id:"banana"},{id:"orange"}],"tab-id":"fruit"},{title0:e(({value:a,isActive:i})=>[s("apple")]),title1:e(({value:a,isActive:i})=>[s("banana")]),title2:e(({value:a,isActive:i})=>[s("orange")]),tab0:e(({value:a,isActive:i})=>[J]),tab1:e(({value:a,isActive:i})=>[U]),tab2:e(({value:a,isActive:i})=>[M]),_:1}),q,l(t,{id:"117",data:[{id:"apple"},{id:"banana"},{id:"orange"}]},{title0:e(({value:a,isActive:i})=>[s("apple")]),title1:e(({value:a,isActive:i})=>[s("banana")]),title2:e(({value:a,isActive:i})=>[s("orange")]),tab0:e(({value:a,isActive:i})=>[z]),tab1:e(({value:a,isActive:i})=>[F]),tab2:e(({value:a,isActive:i})=>[G]),_:1}),H])}const R=u(b,[["render",K],["__file","tabs.html.vue"]]);export{R as default};
