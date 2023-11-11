99d822a76e72cca5dc577a4af71c83a2bac4be55
Mister-Hope <mister-hope@outlook.com>
2023-11-11 02:30:47 +0000

    Deploying to netlify from @ vuepress-theme-hope/vuepress-theme-hope@623a32159025d403841b1f0c302288b28d66aef1 🚀

---
+++ assets/demo.html-e77742ba.js
@@ -0,0 +1,99 @@
+import{_ as i}from"./logo-5b4cc875.js";import{_ as u,r as o,o as d,c as p,a as c,e as t,w as s,b as n,d as a}from"./app-fe94cd90.js";const r={},k=n("h1",{id:"一级标题",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#一级标题","aria-hidden":"true"},"#"),a(" 一级标题")],-1),m=n("h2",{id:"二级标题",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#二级标题","aria-hidden":"true"},"#"),a(" 二级标题")],-1),_=n("h3",{id:"三级标题",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#三级标题","aria-hidden":"true"},"#"),a(" 三级标题")],-1),h=n("h4",{id:"四级标题",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#四级标题","aria-hidden":"true"},"#"),a(" 四级标题")],-1),b=n("h5",{id:"五级标题",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#五级标题","aria-hidden":"true"},"#"),a(" 五级标题")],-1),g=n("h6",{id:"六级标题",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#六级标题","aria-hidden":"true"},"#"),a(" 六级标题")],-1),v=n("div",{class:"language-markdown line-numbers-mode","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,[n("span",{class:"token title important"},[n("span",{class:"token punctuation"},"#"),a(" 一级标题")]),a(`
+
+`),n("span",{class:"token title important"},[n("span",{class:"token punctuation"},"##"),a(" 二级标题")]),a(`
+
+`),n("span",{class:"token title important"},[n("span",{class:"token punctuation"},"###"),a(" 三级标题")]),a(`
+
+`),n("span",{class:"token title important"},[n("span",{class:"token punctuation"},"####"),a(" 四级标题")]),a(`
+
+`),n("span",{class:"token title important"},[n("span",{class:"token punctuation"},"#####"),a(" 五级标题")]),a(`
+
+`),n("span",{class:"token title important"},[n("span",{class:"token punctuation"},"######"),a(" 六级标题")]),a(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),f=n("p",null,[a("这句话里拥有"),n("strong",null,"加粗"),a("、"),n("em",null,"倾斜"),a("和"),n("s",null,"删除")],-1),w=n("div",{class:"language-markdown","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,[a("这句话里拥有"),n("span",{class:"token bold"},[n("span",{class:"token punctuation"},"**"),n("span",{class:"token content"},"加粗"),n("span",{class:"token punctuation"},"**")]),a("、"),n("span",{class:"token italic"},[n("span",{class:"token punctuation"},"*"),n("span",{class:"token content"},"倾斜"),n("span",{class:"token punctuation"},"*")]),a("和"),n("span",{class:"token strike"},[n("span",{class:"token punctuation"},"~~"),n("span",{class:"token content"},"删除"),n("span",{class:"token punctuation"},"~~")]),a(`
+`)])])],-1),x=n("p",null,"这是一个段落。",-1),y=n("p",null,"这是另一个段落。",-1),j=n("div",{class:"language-markdown","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,`这是一个段落。
+
+这是另一个段落。
+`)])],-1),M=n("p",null,[a("这是一句话不过我要在这里"),n("br"),a(" 换行且"),n("br"),a(" 再次换行")],-1),E=n("div",{class:"language-markdown","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,`这是一句话不过我要在这里  
+换行且\\
+再次换行
+`)])],-1),L=n("div",{class:"hint-container tip"},[n("p",{class:"hint-container-title"},"提示"),n("p",null,[a("上方的代码中 "),n("code",null,"这里"),a(" 后面有两个空格")])],-1),z=n("blockquote",null,[n("p",null,"引用也可以连用"),n("blockquote",null,[n("p",null,"可以添加额外的大于号制造更深的引用")])],-1),q=n("div",{class:"language-markdown","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,[n("span",{class:"token blockquote punctuation"},">"),a(` 引用也可以连用
+`),n("span",{class:"token blockquote punctuation"},">"),a(`
+`),n("span",{class:"token blockquote punctuation"},"> >"),a(` 可以添加额外的大于号制造更深的引用
+`)])])],-1),D=n("ul",null,[n("li",null,[n("p",null,"无序列表项")]),n("li",null,[n("p",null,"无序列表项"),n("ul",null,[n("li",null,[n("p",null,"列表中的列表项"),n("ul",null,[n("li",null,"更多的列表项"),n("li",null,"更多的列表项"),n("li",null,"更多的列表项")])]),n("li",null,[n("p",null,"列表中的长列表项，这个列表项很长。"),n("p",null,"而且由很多个段落构成。")])])]),n("li",null,[n("p",null,"无序列表项")])],-1),R=n("div",{class:"language-markdown line-numbers-mode","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,[n("span",{class:"token list punctuation"},"-"),a(` 无序列表项
+`),n("span",{class:"token list punctuation"},"-"),a(` 无序列表项
+
+  `),n("span",{class:"token list punctuation"},"-"),a(` 列表中的列表项
+    `),n("span",{class:"token list punctuation"},"-"),a(` 更多的列表项
+    `),n("span",{class:"token list punctuation"},"-"),a(` 更多的列表项
+    `),n("span",{class:"token list punctuation"},"-"),a(` 更多的列表项
+  `),n("span",{class:"token list punctuation"},"-"),a(` 列表中的长列表项，这个列表项很长。
+
+`),n("span",{class:"token code keyword"},"    而且由很多个段落构成。"),a(`
+
+`),n("span",{class:"token list punctuation"},"-"),a(` 无序列表项
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),V=n("ol",null,[n("li",null,"有序列表第一项"),n("li",null,[a("有序列表第二项"),n("br"),a(" 第二项的需要换行"),n("br"),a(" 再次换行")]),n("li",null,"有序列表第三项")],-1),N=n("div",{class:"language-markdown","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,[n("span",{class:"token list punctuation"},"1."),a(` 有序列表第一项
+`),n("span",{class:"token list punctuation"},"1."),a(` 有序列表第二项  
+   第二项的需要换行\\
+   再次换行
+`),n("span",{class:"token list punctuation"},"1."),a(` 有序列表第三项
+`)])])],-1),S=n("div",{class:"hint-container tip"},[n("p",{class:"hint-container-title"},"提示"),n("p",null,[a("上方的代码中"),n("code",null,"换行"),a("后面有也两个空格")])],-1),A=n("hr",null,null,-1),B=n("div",{class:"language-markdown","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,[n("span",{class:"token hr punctuation"},"---"),a(`
+`)])])],-1),C=n("p",null,[n("a",{href:"/v2/contribution"},"根目录访问贡献指南")],-1),T=n("div",{class:"language-markdown","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,[n("span",{class:"token url"},[a("["),n("span",{class:"token content"},"根目录访问主页"),a("]("),n("span",{class:"token url"},"/v2/"),a(")")]),a(`
+
+`),n("span",{class:"token url"},[a("["),n("span",{class:"token content"},"相对路径访问主页"),a("]("),n("span",{class:"token url"},"../../README.md"),a(")")]),a(`
+
+`),n("span",{class:"token url"},[a("["),n("span",{class:"token content"},"根目录访问贡献指南"),a("]("),n("span",{class:"token url"},"/v2/contribution"),a(")")]),a(`
+
+`),n("span",{class:"token url"},[a("["),n("span",{class:"token content"},"相对路径访问贡献指南"),a("]("),n("span",{class:"token url"},"../../contribution.md"),a(")")]),a(`
+`)])])],-1),H=n("div",{class:"language-markdown","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,[n("span",{class:"token list punctuation"},"-"),a(),n("span",{class:"token url"},[a("["),n("span",{class:"token content"},"相对路径访问，兼容 Markdown 编写时相互跳转"),a("]("),n("span",{class:"token url"},"../../README.md"),a(")")]),a(`
+
+`),n("span",{class:"token list punctuation"},"-"),a(),n("span",{class:"token url"},[a("["),n("span",{class:"token content"},"根目录访问主页 2"),a("]("),n("span",{class:"token url"},"/README.md"),a(")")]),a(`
+
+`),n("span",{class:"token list punctuation"},"-"),a(),n("span",{class:"token url"},[a("["),n("span",{class:"token content"},"HTML 形式"),a("]("),n("span",{class:"token url"},"../../index.html"),a(")")]),a(`
+`)])])],-1),P=n("figure",null,[n("img",{src:i,alt:"Logo",tabindex:"0",loading:"lazy"}),n("figcaption",null,"Logo")],-1),F=n("div",{class:"language-markdown","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,[n("span",{class:"token url"},[n("span",{class:"token operator"},"!"),a("["),n("span",{class:"token content"},"Logo"),a("]("),n("span",{class:"token url"},"/logo.png"),a(")")]),a(`
+`)])])],-1),G=n("p",null,"经典方式:",-1),I=n("p",null,"😉 😢 😆 😋",-1),J=n("p",null,"简写:",-1),K=n("p",null,"😎 😃 :* 😦 😃 😦 😉",-1),O=n("div",{class:"language-markdown","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,`经典方式:
+
+:wink: :cry: :laughing: :yum:
+
+简写:
+
+8-) :) :\\* :( :-) :-( ;)
+`)])],-1),Q={class:"hint-container tip"},U=n("p",{class:"hint-container-title"},"提示",-1),W=n("table",null,[n("thead",null,[n("tr",null,[n("th",{style:{"text-align":"center"}},"居中"),n("th",{style:{"text-align":"right"}},"右对齐"),n("th",{style:{"text-align":"left"}},"左对齐")])]),n("tbody",null,[n("tr",null,[n("td",{style:{"text-align":"center"}},[a("居中使用"),n("code",null,":-:")]),n("td",{style:{"text-align":"right"}},[a("右对齐使用"),n("code",null,"-:")]),n("td",{style:{"text-align":"left"}},[a("左对齐使用"),n("code",null,":-")])]),n("tr",null,[n("td",{style:{"text-align":"center"}},"b"),n("td",{style:{"text-align":"right"}},"aaaaaaaaa"),n("td",{style:{"text-align":"left"}},"aaaa")]),n("tr",null,[n("td",{style:{"text-align":"center"}},"c"),n("td",{style:{"text-align":"right"}},"aaaa"),n("td",{style:{"text-align":"left"}},"a")])])],-1),X=n("div",{class:"language-markdown","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,[n("span",{class:"token table"},[n("span",{class:"token table-header-row"},[n("span",{class:"token punctuation"},"|"),n("span",{class:"token table-header important"},"     居中      "),n("span",{class:"token punctuation"},"|"),n("span",{class:"token table-header important"},"         右对齐 "),n("span",{class:"token punctuation"},"|"),n("span",{class:"token table-header important"}," 左对齐         "),n("span",{class:"token punctuation"},"|"),a(`
+`)]),n("span",{class:"token table-line"},[n("span",{class:"token punctuation"},"|"),a(),n("span",{class:"token punctuation"},":-----------:"),a(),n("span",{class:"token punctuation"},"|"),a(),n("span",{class:"token punctuation"},"-------------:"),a(),n("span",{class:"token punctuation"},"|"),a(),n("span",{class:"token punctuation"},":-------------"),a(),n("span",{class:"token punctuation"},"|"),a(`
+`)]),n("span",{class:"token table-data-rows"},[n("span",{class:"token punctuation"},"|"),n("span",{class:"token table-data"},[a(" 居中使用"),n("span",{class:"token code-snippet code keyword"},"`:-:`"),a()]),n("span",{class:"token punctuation"},"|"),n("span",{class:"token table-data"},[a(" 右对齐使用"),n("span",{class:"token code-snippet code keyword"},"`-:`"),a()]),n("span",{class:"token punctuation"},"|"),n("span",{class:"token table-data"},[a(" 左对齐使用"),n("span",{class:"token code-snippet code keyword"},"`:-`"),a()]),n("span",{class:"token punctuation"},"|"),a(`
+`),n("span",{class:"token punctuation"},"|"),n("span",{class:"token table-data"},"       b       "),n("span",{class:"token punctuation"},"|"),n("span",{class:"token table-data"},"      aaaaaaaaa "),n("span",{class:"token punctuation"},"|"),n("span",{class:"token table-data"}," aaaa           "),n("span",{class:"token punctuation"},"|"),a(`
+`),n("span",{class:"token punctuation"},"|"),n("span",{class:"token table-data"},"       c       "),n("span",{class:"token punctuation"},"|"),n("span",{class:"token table-data"},"           aaaa "),n("span",{class:"token punctuation"},"|"),n("span",{class:"token table-data"}," a              "),n("span",{class:"token punctuation"},"|"),a(`
+`)])])])])],-1),Y=n("p",null,[a("行内代码效果: "),n("code",null,"code")],-1),Z=n("p",null,"缩进代码:",-1),$=n("pre",null,[n("code",null,`// Some comments
+line 1 of code
+line 2 of code
+line 3 of code
+`)],-1),nn=n("p",null,"块级代码",-1),an=n("div",{class:"language-markdown","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,`Sample text here...
+`)])],-1),sn=n("p",null,"高亮格式:",-1),tn=n("div",{class:"language-javascript","data-ext":"js"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token keyword"},"var"),a(),n("span",{class:"token function-variable function"},"foo"),a(),n("span",{class:"token operator"},"="),a(),n("span",{class:"token keyword"},"function"),a(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},"bar"),n("span",{class:"token punctuation"},")"),a(),n("span",{class:"token punctuation"},"{"),a(`
+  `),n("span",{class:"token keyword"},"return"),a(" bar"),n("span",{class:"token operator"},"++"),n("span",{class:"token punctuation"},";"),a(`
+`),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},";"),a(`
+
+console`),n("span",{class:"token punctuation"},"."),n("span",{class:"token function"},"log"),n("span",{class:"token punctuation"},"("),n("span",{class:"token function"},"foo"),n("span",{class:"token punctuation"},"("),n("span",{class:"token number"},"5"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},";"),a(`
+`)])])],-1),en=n("div",{class:"language-markdown line-numbers-mode","data-ext":"md"},[n("pre",{class:"language-markdown"},[n("code",null,[a("行内代码效果: "),n("span",{class:"token code-snippet code keyword"},"`code`"),a(`
+
+缩进代码:
+
+`),n("span",{class:"token code keyword"},`    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code`),a(`
+
+块级代码
+
+`),n("span",{class:"token code"},[n("span",{class:"token punctuation"},"```"),n("span",{class:"token code-language"},"md"),a(`
+`),n("span",{class:"token code-block language-md language-md"},"Sample text here..."),a(`
+`),n("span",{class:"token punctuation"},"```")]),a(`
+
+高亮格式:
+
+`),n("span",{class:"token code"},[n("span",{class:"token punctuation"},"```"),n("span",{class:"token code-language"},"js"),a(`
+`),n("span",{class:"token code-block language-js language-js"},[n("span",{class:"token keyword"},"var"),a(),n("span",{class:"token function-variable function"},"foo"),a(),n("span",{class:"token operator"},"="),a(),n("span",{class:"token keyword"},"function"),a(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},"bar"),n("span",{class:"token punctuation"},")"),a(),n("span",{class:"token punctuation"},"{"),a(`
+  `),n("span",{class:"token keyword"},"return"),a(" bar"),n("span",{class:"token operator"},"++"),n("span",{class:"token punctuation"},";"),a(`
+`),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},";"),a(`
+
+console`),n("span",{class:"token punctuation"},"."),n("span",{class:"token function"},"log"),n("span",{class:"token punctuation"},"("),n("span",{class:"token function"},"foo"),n("span",{class:"token punctuation"},"("),n("span",{class:"token number"},"5"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},";")]),a(`
+`),n("span",{class:"token punctuation"},"```")]),a(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function ln(on,cn){const e=o("MdDemo"),l=o("RouterLink");return d(),p("div",null,[c(" markdownlint-disable "),t(e,{title:"标题",id:"md-demo-1"},{default:s(()=>[k,m,_,h,b,g]),code:s(()=>[v]),_:1}),c(" markdownlint-restore "),t(e,{title:"文本",id:"md-demo-23"},{default:s(()=>[f]),code:s(()=>[w]),_:1}),t(e,{title:"段落",id:"md-demo-29"},{default:s(()=>[x,y]),code:s(()=>[j]),_:1}),t(e,{title:"换行",id:"md-demo-38"},{default:s(()=>[M]),code:s(()=>[E]),_:1}),L,t(e,{title:"引用",id:"md-demo-49"},{default:s(()=>[z]),code:s(()=>[q]),_:1}),t(e,{title:"无序列表",id:"md-demo-62"},{default:s(()=>[D]),code:s(()=>[R]),_:1}),t(e,{title:"有序列表",id:"md-demo-114"},{default:s(()=>[V]),code:s(()=>[N]),_:1}),S,t(e,{title:"分割线",id:"md-demo-139"},{default:s(()=>[A]),code:s(()=>[B]),_:1}),t(e,{title:"链接",id:"md-demo-143"},{default:s(()=>[n("p",null,[t(l,{to:"/v2/"},{default:s(()=>[a("根目录访问主页")]),_:1})]),n("p",null,[t(l,{to:"/zh/"},{default:s(()=>[a("相对路径访问主页")]),_:1})]),C,n("p",null,[t(l,{to:"/zh/contribution.html"},{default:s(()=>[a("相对路径访问贡献指南")]),_:1})])]),code:s(()=>[T]),_:1}),t(e,{title:"VuePress 兼容的其他链接",id:"md-demo-158"},{default:s(()=>[n("ul",null,[n("li",null,[n("p",null,[t(l,{to:"/zh/"},{default:s(()=>[a("相对路径访问，兼容 Markdown 编写时相互跳转")]),_:1})])]),n("li",null,[n("p",null,[t(l,{to:"/"},{default:s(()=>[a("根目录访问主页 2")]),_:1})])]),n("li",null,[n("p",null,[t(l,{to:"/zh/index.html"},{default:s(()=>[a("HTML 形式")]),_:1})])])])]),code:s(()=>[H]),_:1}),t(e,{title:"图片",id:"md-demo-178"},{default:s(()=>[P]),code:s(()=>[F]),_:1}),t(e,{title:"Emoji",id:"md-demo-184"},{default:s(()=>[G,I,J,K]),code:s(()=>[O]),_:1}),n("div",Q,[U,n("p",null,[a("更多详见"),t(l,{to:"/zh/cookbook/markdown/emoji/"},{default:s(()=>[a("emoji 列表")]),_:1})])]),t(e,{title:"表格",id:"md-demo-204"},{default:s(()=>[W]),code:s(()=>[X]),_:1}),t(e,{title:"代码",id:"md-demo-257"},{default:s(()=>[Y,Z,$,nn,an,sn,tn]),code:s(()=>[en]),_:1})])}const pn=u(r,[["render",ln],["__file","demo.html.vue"]]);export{pn as default};
